5b00851805c99144b3e95bf5f587793e42657666
MauriceFreund <maurice-freund@gmx.de>
2020-12-12 16:07:19 +0100

    Init commit for secureuser

---
+++ secureuser/src/main/java/com/mauricefreund/secureuser/SecureUserApplication.java
@@ -0,0 +1,13 @@
+package com.mauricefreund.secureuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureUserApplication.class, args);
+	}
+
+}
---
+++ secureuser/src/test/java/com/mauricefreund/secureuser/SecureUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricefreund.secureuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
