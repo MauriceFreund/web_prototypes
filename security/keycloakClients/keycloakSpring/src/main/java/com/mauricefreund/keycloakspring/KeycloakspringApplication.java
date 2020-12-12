69f4c74d708ba3fc87c6f00d032405e592b8f56c
MauriceFreund <maurice-freund@gmx.de>
2020-12-12 19:49:13 +0100

    Init commit for keycloakSpring

---
+++ security/keycloakClients/keycloakSpring/src/main/java/com/mauricefreund/keycloakspring/KeycloakspringApplication.java
@@ -0,0 +1,13 @@
+package com.mauricefreund.keycloakspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakspringApplication.class, args);
+	}
+
+}
